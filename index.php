588b68474f675353c8c40e3800634452695d152e
ZenonStella <stellazenon@gmail.com>
2022-06-28 09:28:22 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+$day = 1;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container mt-5">
+        <table class="table table-bordered table-hover border border-dark table-striped-columns">
+            <thead class="table-dark">
+                <tr>
+                    <th>Lundi</th>
+                    <th>Mardi</th>
+                    <th>Mercredi</th>
+                    <th>Jeudi</th>
+                    <th>Vendredi</th>
+                    <th>Samedi</th>
+                    <th>Dimanche</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php for ($i = 1; $i <= 6; $i++) { ?>
+                    <tr>
+                        <?php for ($a = 1; $a <= 7; $a++) { ?>
+                            <th scope="row"><?= $day ?></th>
+
+                        <?php
+                            $day++;
+                        } ?>
+                    </tr>
+                <?php  } ?>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
+</html>
