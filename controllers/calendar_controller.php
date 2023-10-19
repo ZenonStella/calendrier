4067a6af34e4730a22b3a192642b9539264f6633
ZenonStella <stellazenon@gmail.com>
2023-10-19 18:38:53 +0200

    mise à jour pour l'année CDUI

--- controllers/calendar_controller.php
+++ controllers/calendar_controller.php
@@ -57,9 +57,10 @@ function getSpecialDays($year)
         strtotime("$year-5-1") => 'Fête du travail',
         strtotime("$year-5-8") => 'Victoire des allies',
         strtotime("$year-7-14") => 'Fete nationale',
-        strtotime("$year-8-15") => 'Assomption',
+        strtotime("$year-8-15") => 'A5ssomption',
         strtotime("$year-11-1") => 'Toussaint',
         strtotime("$year-11-11") => 'Armistice',
+
         strtotime("$year-12-25") => 'Noël',
         // Jour feries qui dependent du jour de pâque
         strtotime("$easterDay + 1 days") => 'Lundi de paques',
@@ -67,13 +68,74 @@ function getSpecialDays($year)
         strtotime("$easterDay + 50 days") => 'Pentecote',
 
         // annivs apprenant LHP8
-        strtotime("$year-10-11") => 'Jordan <i class="bi bi-balloon-fill"></i>',
-        strtotime("$year-9-21") => 'Sophie <i class="bi bi-balloon-heart-fill"></i>',
-        strtotime("$year-12-21") => 'Alexandre <i class="bi bi-balloon-fill"></i>',
-        strtotime("$year-12-20") => 'Mickael <i class="bi bi-balloon-fill"></i>',
-        strtotime("$year-3-23") => 'Anousone <i class="bi bi-balloon-fill"></i>',
-        strtotime("$year-2-21") => 'Valentin <i class="bi bi-balloon-fill"></i>',
-        strtotime("$year-4-10") => 'Stella <i class="bi bi-balloon-heart-fill"></i>'
+        strtotime("2023-10-16") => '16 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-10-17") => '17 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-10-18") => '18 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-10-19") => '19 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-10-20") => '20 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-11-13") => '13 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-11-14") => '14 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-11-15") => '15 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-11-16") => '16 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-11-17") => '17 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-12-11") => '11 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-12-12") => '12 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-12-13") => '13 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-12-14") => '14 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2023-12-15") => '15 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-01-15") => '15 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-01-16") => '16 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-01-17") => '17 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-01-18") => '18 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-01-19") => '19 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-02-12") => '12 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-02-13") => '13 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-02-14") => '14 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-02-15") => '15 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-02-16") => '16 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-03-18") => '18 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-03-19") => '19 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-03-20") => '20 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-03-21") => '21 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-03-22") => '22 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-04-08") => '23 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-04-09") => '09 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-04-10") => '10 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-04-11") => '11 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-04-12") => '12 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-05-13") => '13 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-05-14") => '14 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-05-15") => '15 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-05-16") => '16 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-05-17") => '17 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-06-10") => '10 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-06-11") => '11 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-06-12") => '12 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-06-13") => '13 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-06-14") => '14 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-07-15") => '15 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-07-16") => '16 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-07-17") => '17 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-07-18") => '18 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-07-19") => '19 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-08-26") => '26 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-08-27") => '27 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-08-28") => '28 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-08-29") => '29 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-08-30") => '30 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-09-09") => '09 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-09-10") => '10 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-09-11") => '11 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-09-12") => '12 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-09-13") => '13 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-10-14") => '14 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-10-15") => '15 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-10-16") => '16 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-10-17") => '17 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-10-18") => '18 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-10-21") => '21 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-10-22") => '22 <i class="bi bi-backpack3-fill"></i>',
+        strtotime("2024-10-23") => '23 <i class="bi bi-backpack3-fill"></i>'
     ];
     return $specialDays;
 }
@@ -82,7 +144,7 @@ function createCase($firstCaseTimestamp, $caseNumber, $month, $arraySpecialDays)
 {
     $timestamp = strtotime(date('Y-m-d', $firstCaseTimestamp) . '+' . ($caseNumber - 1) . 'days');
     if (isset($arraySpecialDays[$timestamp])) {
-        return '<div class="feries text-center bg-success border border-dark">' . $arraySpecialDays[$timestamp] . '</div>';
+        return '<div class="feries text-center bg-danger border border-dark">' . $arraySpecialDays[$timestamp] . '</div>';
     } elseif (date('Y-m-d', $timestamp) == date('Y-m-d')) {
         return '<div class="text-center border border-dark bg-info">' . date('j', $timestamp) . '</div>';
     } elseif (date('n', $timestamp) == $month) {
